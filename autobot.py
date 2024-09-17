956e28e02ab27d1989df5e031310c11524955ebc
unknown <wilson121yer@gmail.com>
2024-09-17 14:28:17 -0500

    change

--- autobot.py
+++ autobot.py
@@ -210,7 +210,7 @@ class ManageInsta:
     
     def _webdriver(self) -> webdriver.Firefox:
         options = webdriver.FirefoxOptions()
-        #options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         path_extention = os.path.abspath("autobot.py").replace("autobot.py", "captchaSolver")
